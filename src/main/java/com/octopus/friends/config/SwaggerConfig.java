c0edfb7fee2e58396e45570748e2ee638470e637
sudhdkso <waga1009@gmail.com>
2022-09-13 15:06:04 +0900

    [#1] setting:초기 설정

---
+++ src/main/java/com/octopus/friends/config/SwaggerConfig.java
@@ -0,0 +1,36 @@
+package com.octopus.friends.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+
+@Configuration
+@RequiredArgsConstructor
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.OAS_30)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.octopus.friends.controller"))
+                .paths(PathSelectors.ant("/api/**"))
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("play with friend")
+                .description("API docs")
+                .version("1.0")
+                .build();
+    }
+
+}
