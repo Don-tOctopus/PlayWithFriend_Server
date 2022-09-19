4c5551530cf6756d6ff685f6ed16e6e88de150f4
uzhjd <73466440+uzhjd@users.noreply.github.com>
2022-09-19 21:42:28 +0900

    [#15] feat: 화상채팅 socket연결

---
+++ src/main/java/com/octopus/friends/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.octopus.friends.utils;
+
+public class Constants {
+    //public static final String API_URL = "https://";
+    public static final String API_URL = "http://localhost:8000";
+
+    public static final String API_URL_DEV = "http://localhost:8000";
+
+    //public static final String ML_API_URL =  "http://52.78.40.40:5000";
+    public static final String ML_API_URL = "http://localhost:5000";
+
+
+}
