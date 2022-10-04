520063acc258a8c435aa1e815892dcf138d645e0
sudhdkso <waga1009@gmail.com>
2022-10-04 22:37:58 +0900

    [#37] feat: Spring Security 적용

--- src/main/java/com/octopus/friends/FriendsApplication.java
+++ src/main/java/com/octopus/friends/FriendsApplication.java
@@ -6,9 +6,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
-@SpringBootApplication(scanBasePackages = {"com.octopus.friends","com.octopus.friends.repository"})
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class,
+        scanBasePackages = {"com.octopus.friends","com.octopus.friends.repository"})
 public class FriendsApplication {
 
     public static void main(String[] args) {
