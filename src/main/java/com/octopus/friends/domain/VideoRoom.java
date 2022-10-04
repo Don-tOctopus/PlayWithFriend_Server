61612451393fc499f0dbf1a31448bdcd0f2aaa3b
uzhjd <73466440+uzhjd@users.noreply.github.com>
2022-10-04 13:20:51 +0900

    [#15] feat: axios 오류 수정 및 user Domain_role 추가

---
+++ src/main/java/com/octopus/friends/domain/VideoRoom.java
@@ -0,0 +1,15 @@
+package com.octopus.friends.domain;
+
+/**
+ * 패키지명 com.octopus.friends.domain
+ * 클래스명 VideoRoom
+ * 클래스설명
+ * 작성일 2022-10-04
+ * 
+ * @author 남유정
+ * @version 1.0
+ * [수정내용]
+ * 예시) [2022-09-17] 주석추가 - 남유정
+ */
+public class VideoRoom {
+}
